647a2750426240a07118511b5d832561e94b19bd
Ryan <ryan@ryantoronto.com>
2011-08-18 22:33:23 -0400

    version, real domain, and description fix

--- recipecan-recipes.php
+++ recipecan-recipes.php
@@ -2,13 +2,13 @@
 /*
   Plugin Name: RecipeCan Recipes
   Plugin URI: http://www.recipecan.com
-  Description: A wordpress plugin to that organizes recipes for your blog.
-  Version: 0.1
+  Description: A WordPress plugin that organizes recipes on your blog.
+  Version: 0.1.1
   Author: Ryan (ryanto)
   Author URI: http://www.recipecan.com
  */
 
-define('RECIPECAN_VERSION', '0.1');
+define('RECIPECAN_VERSION', '0.1.1');
 
 
 $recipecan_options = array(
@@ -16,8 +16,8 @@ $recipecan_options = array(
     'plugin_version' => RECIPECAN_VERSION,
     'path' => dirname(__FILE__),
     'request' => $_REQUEST,
-    'api_server' => 'www.recipecan.dev',
-    'image_server' => 'www.recipecan.dev',
+    'api_server' => 'www.recipecan.com',
+    'image_server' => 'www.recipecan.com',
     'api_version' => 'v1',
     'prefix' => 'recipecan_',
     'register_global_names' => array('recipes', 'myrecipes', 'recipecan')
